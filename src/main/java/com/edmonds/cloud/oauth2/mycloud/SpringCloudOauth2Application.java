7ba4d819d578a2b4b23191cfc273e2c05db6e92f
edmonds <fengt4553@gmail.com>
2021-12-07 21:11:44 +0800

    second commit

---
+++ src/main/java/com/edmonds/cloud/oauth2/mycloud/SpringCloudOauth2Application.java
@@ -0,0 +1,13 @@
+package com.edmonds.cloud.oauth2.mycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/edmonds/cloud/oauth2/mycloud/SpringCloudOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmonds.cloud.oauth2.mycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
